ba7ea1bb71f5e02f6e89264e2e538f1227921fd1
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-09-06 11:14:12 +0300

    ап

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
-const img = document.querySelector('.img');
+const img = document.querySelector('.form');
 console.log(img);
+
+img.addEventListener('submit', onSearchForm);
+
+function onSearchForm(e) {
+  e.preventDefault();
+  page = 1;
+  query = e.currentTarget.input.value;
+  if (query === '') {
+    console.log(
+      'The search string cannot be empty. Please specify your search query.'
+    );
+    return;
+  }
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      if (data.totalHits === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        gallery.innerHTML = '';
+        renderMarkup(data.hits);
+        lightbox.refresh();
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        searchForm.disable();
+        if (data.totalHits > perPage) {
+          loadMoreBtn.classList.remove('is-hidden');
+        }
+      }
+    })
+    .catch(error => console.log(error));
+}
